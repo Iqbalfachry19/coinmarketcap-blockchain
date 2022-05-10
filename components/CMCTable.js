0f80c2c2cf054385067d0737eed8ab6fca345f5c
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-10 13:30:33 +0700

    hi

---
+++ components/CMCTable.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CMCTable = () => {
+  return <div>CMCTable</div>
+}
+
+export default CMCTable
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl="https://tortdzkfheda.usemoralis.com:2053/server"
+      appId="HvjaZwsGwJSpzv1YDzBL9q8Pq2ba4DROiz8SVyK3"
+    >
+      {' '}
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
