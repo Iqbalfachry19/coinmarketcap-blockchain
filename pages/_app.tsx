7ac0d97726a574ceb314236a064f57dd6834df71
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-10 13:55:37 +0700

    hi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,9 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_SERVER}
-      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER!}
+      appId={process.env.NEXT_PUBLIC_APP_ID!}
     >
-      {' '}
       <Component {...pageProps} />
     </MoralisProvider>
   )
