09bb58e4ae58bf53388612e7548391ef398d8820
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-10 14:53:10 +0700

    hi

---
+++ components/cmc-table/CmcTableRow.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const cmcTableRow = () => {
+  return <div>cmcTableRow</div>
+}
+
+export default cmcTableRow
---
+++ components/cmc-table/CmcTableheader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CmcTableheader = () => {
+  return <div>CmcTableheader</div>
+}
+
+export default CmcTableheader
